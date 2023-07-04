af2ca9545b21b27e19f314a5f90bae02b0da54ba
Farouk Sabiou <sfarouk298@gmail.com>
2023-07-04 16:27:17 +0100

    Refactor codebase to use custom theme colors
    - Replaced hard-coded color codes with named references in the custom theme.
    - Added CustomThemeColors class to define the custom theme colors.
    - Updated RemitConnectTheme and CustomTheme functions to use the custom theme colors.
    - Updated the color definitions in CustomDesignSystemTheme.
    - Optimized the code by removing duplicate color definitions.
    - Updated the documentation to reflect the changes made to the custom theme.

--- app/src/main/java/com/godi/remitconnect/ui/theme/Color.kt
+++ app/src/main/java/com/godi/remitconnect/ui/theme/Color.kt
@@ -4,12 +4,10 @@ import androidx.compose.ui.graphics.Color
 
 val Primary70 = Color(0xFF08A075)
 val Primary100 = Color(0xFF007554)
-val Primary15 = Color(0xFFC8EAE1)
 val Primary05 = Color(0xFFEDF8F5)
 val Primary06 = Color(0xFFF8FCFB)
 
-val Gray100 = Color(0xFF00122C)
-val Gray50 = Color(0xFF7F8895)
 val Gray00 = Color(0xFFFFFFFF)
 
 val Accent100 = Color(0xFFFFCF00)
+
--- app/src/main/java/com/godi/remitconnect/ui/theme/Theme.kt
+++ app/src/main/java/com/godi/remitconnect/ui/theme/Theme.kt
@@ -3,23 +3,19 @@ package com.godi.remitconnect.ui.theme
 import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.godi.remitconnect.ui.theme.CustomDesignSystemTheme.CustomThemeColors
+import com.godi.remitconnect.ui.theme.CustomDesignSystemTheme.LocalCustomThemeColors
 import com.godi.remitconnect.ui.theme.typography.typography
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Primary100,
-    secondary = Gray100,
-    tertiary = Accent100,
-    background = Gray00,
-    onPrimary = Color.White,
-)
 
 private val LightColorScheme = lightColorScheme(
     primary = Primary100,
@@ -37,20 +33,94 @@ fun RemitConnectTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) DarkColorScheme else LightColorScheme
+    val colors = LightColorScheme
 
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colors.primary.toArgb()
+            window.statusBarColor = Color.White.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
-
     MaterialTheme(
         colorScheme = colors,
         typography = typography,
         content = content
     )
+}
+
+object CustomDesignSystemTheme {
+    /**
+     * Custom theme colors used in the RemitConnect app.
+     */
+    @Immutable
+    data class CustomThemeColors(
+        val midnightBlue: Color,
+        val paleMint: Color,
+        val mintGreen: Color,
+        val babyBlue: Color,
+        val slateGray: Color,
+        val duskGray: Color,
+        val ceruleanBlue: Color,
+        val silverGrey: Color,
+        val emeraldGreen: Color,
+        val forestGreen: Color,
+        val slateBlue: Color,
+        val silverGray: Color,
+    )
+
+    /**
+     * Default instance of the custom theme colors.
+     */
+    val customThemeColors = CustomThemeColors(
+        midnightBlue = Color(0xFF00122C),
+        paleMint = Color(0xFFEDF8F5),
+        mintGreen = Color(0xFFC8EAE1),
+        babyBlue = Color(0xFFEAF6FC),
+        slateGray = Color(0xFF7F8895),
+        duskGray = Color(0xFF7F8895),
+        ceruleanBlue = Color(0xFF1B98E0),
+        silverGrey = Color(0xFFF2F3F4),
+        emeraldGreen = Color(0xFF00A85A),
+        forestGreen = Color(0xFF055C00),
+        slateBlue = Color(0xFF404D61),
+        silverGray = Color(0xFFBFC3CA),
+    )
+
+    /**
+     * Local provider key for accessing the custom theme colors.
+     */
+    val LocalCustomThemeColors = staticCompositionLocalOf {
+        customThemeColors
+    }
+}
+
+/**
+ * Composable function for applying the custom theme colors to the content.
+ *
+ * @param content The content to apply the custom theme to.
+ */
+@Composable
+fun CustomTheme(
+    content: @Composable () -> Unit
+) {
+    val customThemeColors = CustomDesignSystemTheme.customThemeColors
+
+    CompositionLocalProvider(LocalCustomThemeColors provides customThemeColors) {
+        MaterialTheme(
+            content = content
+        )
+    }
+}
+
+/**
+ * Provides access to the custom theme colors from the current composition.
+ *
+ * Usage: `val customThemeColors = CustomTheme.colors`
+ */
+object CustomTheme {
+    val colors: CustomThemeColors
+        @Composable
+        get() = LocalCustomThemeColors.current
 }
